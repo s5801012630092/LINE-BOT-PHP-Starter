1c26c0a10673e76a311021794295c47843327268
s5801012630092 <31880220+s5801012630092@users.noreply.github.com>
2017-10-30 21:02:08 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,5 @@
 <?php
 
-$proxy = 'fixie:bglYmiLPH45Bx7U';
-$proxyauth = 'velodrome.usefixie.com:80';
 
 $access_token = 'IA5TvSQ5R7vRqjc+rlu0jKcKRjGO6Boiqa6xEIoJKpbaSvGv3tWdsAjaAgB8NKIITv8NtDsPUaE2mzUVQYuwtO31WMCgk7VS8U32JGF8wIzwXZQ2OJlhzoEleQWfDh8VbMym33ckDwzZSsTFLBzJVgdB04t89/1O/w1cDnyilFU=';
 
@@ -14,8 +12,6 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 
 $result = curl_exec($ch);
 curl_close($ch);
