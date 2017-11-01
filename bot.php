c3f693b2d9ddc47abeba23bf71c7eaaad52a9b46
s5801012630092 <31880220+s5801012630092@users.noreply.github.com>
2017-11-01 18:03:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,8 +20,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text = 'Hello'
+				{
+					'type' => 'text',
+					'text' => $text = 'Hello'
+				},
+				{
+					'type' => 'text',
+					'text' => $text = 'May I help you?'
+				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
